a09f8cecbf32543a9908c6bafdd3a42da97c5f4d
MoWagih <mowagih2@gmail.com>
2025-01-01 10:59:27 +0200

    second-commit

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 
-let userName = prompt("please set your name")
 let bill = prompt("Please set total bill");
 let discount = prompt("Please set discount")/100;
 let totalPrice = bill - bill*discount;
-document.write("total price is: " + totalPrice + " for " + userName )
+document.write("total price is: " + totalPrice )
 let tax = 14/100*totalPrice + totalPrice;
 document.write(", Total after Tax: " + tax)
 
